bb1af44b35da02e0c23b55f0f44a2e2b8ab7ff3e
waleedHAlmenawy <waleed.almenawy@outlook.com>
2023-06-14 01:41:17 +0300

    script that creates an alias

--- 0x03-shell_variables_expansions/README.md
+++ 0x03-shell_variables_expansions/README.md
@@ -1,1 +1,2 @@
 <h1>Scripts</h1>
+<p>0-alias : script that creates an alias<br>
---
+++ 0x03-shell_variables_expansions/git.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo $SH_NAME
+echo $SH_CMD
+echo $SH_MSG
+
+touch $SH_NAME
+echo "#!/bin/bash" > $SH_NAME
+echo $SH_CMD >> $SH_NAME
+
+chmod u+x $SH_NAME
+
+echo "<p>$SH_NAME : $SH_MSG<br>" >> README.md
+
+echo $?
+
+git add .
+git commit -m "$SH_MSG"
+git push
